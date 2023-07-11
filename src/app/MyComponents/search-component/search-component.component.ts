8d0f8b5adb74870c4800cea0fb01440c32bfc7e2
Yashika-Shetty <yashika.shetty_19@sakec.ac.in>
2023-07-11 23:27:38 +0530

    Added files for Assignment 2 Day 2 Exercise 2

--- src/app/MyComponents/search-component/search-component.component.ts
+++ src/app/MyComponents/search-component/search-component.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-component',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-component.component.css']
 })
 export class SearchComponentComponent {
+  searchControl = new FormControl();
+  
+  constructor() {
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    ).subscribe(searchQuery => {
+      this.search(searchQuery);
+    });
+  }
 
+  search(query: string) {
+    // Perform search request and update search results
+    console.log('Search query:', query);
+    // Implement your search logic here, such as making an HTTP request and updating the search results on the page.
+  }
 }
