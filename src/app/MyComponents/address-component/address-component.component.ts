1751dc41d82984227ab3c96fb14a739493895032
YashikaSShetty <yashikashetty15@gmail.com>
2023-07-11 10:44:43 +0530

    Added files for Assignment 2 Exercise 5

--- src/app/MyComponents/address-component/address-component.component.ts
+++ src/app/MyComponents/address-component/address-component.component.ts
@@ -7,6 +7,44 @@ import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
   templateUrl: './address-component.component.html',
   styleUrls: ['./address-component.component.css']
 })
-export class AddressComponentComponent {
+export class AddressComponentComponent implements OnInit {
+  addressForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.addressForm = this.formBuilder.group({
+      addresses: this.formBuilder.array([])
+    });
+
+    this.addAddress(); // Add one initial address field
+  }
+
+  get addressControls() {
+    return this.addressForm.get('addresses') as FormArray;
+  }
+
+  addAddress() {
+    const addressFormGroup = this.formBuilder.group({
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      postalCode: ['', Validators.required]
+    });
+
+    this.addressControls.push(addressFormGroup);
+  }
+
+  removeAddress(index: number) {
+    this.addressControls.removeAt(index);
+  }
+
+  onSubmit() {
+    if (this.addressForm.invalid) {
+      return;
+    }
+
+    // Perform actions with the form data, e.g., submit to server
+    console.log(this.addressForm.value);
+  }
 }
